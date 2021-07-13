ead6988e79dcfc5cee2eb5d1a05aecd9f51e0e19
UndefinedIdentifier <987432340@qq.com>
2021-07-13 23:35:23 +0800

    第一课:旋转香蕉

---
+++ lesson1/MyProject2/Source/MyProject2/MyObject.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UObject/NoExportTypes.h"
+#include "MyObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API UMyObject : public UObject
+{
+	GENERATED_BODY()
+public:
+	UMyObject();
+	
+	
+	
+};
